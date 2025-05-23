1ae1b0b396301ee0cadb47c6ca49b6b7362f89ed
Cursed <61657150+CursedDK@users.noreply.github.com>
2025-05-23 08:38:17 +0200

    Update ContactService.ts

--- src/app/services/ContactService.ts
+++ src/app/services/ContactService.ts
@@ -1,3 +1,4 @@
+
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -7,14 +8,13 @@ export class ContactService {
 
 	sendContactForm(data: { name: string; email: string; message: string }) {
 
-		
         var header = { 
             headers: new HttpHeaders({
                 'Content-Type' : 'application/json',    
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
                 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-            });
+            })
         } 
 
 		return this.http.post('https://klimbimgmbh.de/send-email', data, header);
